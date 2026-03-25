1a4e01f93ec99fe1aef653ad2be629e8b4136d08
Torben Abend <torben.abend@gmail.com>
2026-03-25 10:04:01 +0100

    Add new feature: Add new post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,10 +10,34 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+def get_new_id() -> int:
+    return 1 + max(
+        (post["id"] for post in POSTS)
+    )
+
+
+def validate_post_data(post_data: dict[str, str]) -> bool:
+    if "title" not in post_data or "content" not in post_data:
+        return False
+    return True
+
+
+@app.route("/api/posts", methods=["GET"])
 def get_posts():
     return jsonify(POSTS)
 
 
-if __name__ == '__main__':
+@app.route("/api/posts", methods=["POST"])
+def add_post():
+    new_post = request.get_json()
+    if not validate_post_data(new_post):
+        return jsonify(
+            {"error": "Invalid post data: 'title' or 'content' missing"}
+        ), 400
+    new_post["id"] = get_new_id()
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
