0ff847bb3ac32f86558910e1d292301068441e83
Torben Abend <torben.abend@gmail.com>
2026-03-26 11:21:22 +0100

    Add API documentation with swagger

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -17,6 +17,8 @@ Note:
 """
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -25,6 +27,19 @@ POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
+
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 
 def get_new_id() -> int:
